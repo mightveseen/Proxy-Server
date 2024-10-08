d4dca7f7e7656538d8db41163e38684785cc0ff3
Valiantsin <24612920+mightveseen@users.noreply.github.com>
2024-10-08 11:28:38 +0200

    Add changes for 7th task. (#4)

--- index.js
+++ index.js
@@ -11,14 +11,32 @@ async function getApiData(startDate, endDate) {
     return axios.get(apiUrl);
 }
 
+function filterResponse(data) {
+    let result = [];
+    Object.values(data['near_earth_objects']).forEach(date => {
+        date.forEach(meteor => {
+            result.push({
+                id : meteor['id'],
+                name : meteor['name'],
+                diameter: meteor['estimated_diameter']['meters'],
+                is_potentially_hazardous_asteroid: meteor['is_potentially_hazardous_asteroid'],
+                close_approach_date_full: meteor['close_approach_data'][0]['close_approach_date_full'],
+                relative_velocity: meteor['close_approach_data'][0]['relative_velocity']['kilometers_per_second'],
+            });
+        });
+    });
+    return result;
+}
+
 app.get('/api/meteors', async (req, res) => {
     try {
         const { startDate, endDate } = req.query;
-        res.status(200).json((await getApiData(startDate, endDate)).data);
+        const response = filterResponse((await getApiData(startDate, endDate)).data);
+        res.status(200).json(response);
     } catch (error) {
         const message = `Error occured during API call [${error}]`;
-        console.error(message)
-        res.status(400).json({message})
+        console.error(message);
+        res.status(400).json({ message });
     }
 });
 
