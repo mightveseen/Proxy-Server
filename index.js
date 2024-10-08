d54eef6003e721e2764b248cec34c7245ba80af5
mightveseen <mightveseen@gmail.com>
2024-10-08 09:36:04 +0200

    Add changes of 4th task.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 
+const app = express();
 const now = new Date();
 const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
 const apiUrl = `${process.env.API_URL}?start_date=${date}&end_date=${date}&api_key=${process.env.API_KEY}`;
@@ -16,4 +18,6 @@ function getApiData() {
         })
 }
 
-getApiData();
+app.listen(4000, () => {
+    console.log(`Server is running on port ${4000}...`);
+});
