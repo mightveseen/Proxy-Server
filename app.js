b3d6157aa72e124c3eb596ec3d0ca4ae7011f677
mightveseen <mightveseen.sec@gmail.com>
2024-10-10 13:57:36 +0200

    Fixed comments

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-import { config } from 'dotenv';
 import nunjucks from 'nunjucks';
 import express from 'express';
-import meteorDelivery from './delivery/meteor-delivery.js';
+import meteorController from './delivery/meteor-controller.js';
+import { env } from "./config/config.js";
 
 const app = express();
-const port = process.env.SERVER_PORT || 4000;
 
-config();
 nunjucks.configure('views', {
     express: app
 })
 
-app.use(meteorDelivery);
+app.use(meteorController);
 app.use((error, _req, res, _next) => {
     res.status(error.code || 500).render('exception.njk', { message: error.message });
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port} ...`);
+app.listen(env.port, () => {
+    console.log(`Server is running on port ${env.port} ...`);
 });
